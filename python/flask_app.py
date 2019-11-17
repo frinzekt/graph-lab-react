2b00c8f39d0bf6c24523e9432128dab9968f9069
frinzekt <frinzekt@gmail.com>
2019-11-17 23:53:44 +0800

    API routes for 3D surface and 2D visualizations

--- python/flask_app.py
+++ python/flask_app.py
@@ -1,25 +1,105 @@
-import python.helper.algebra
-import python.helper.algebra3d
+
 from flask import Flask
 from flask import jsonify
-
+from flask import request
+import sys
+from helper import algebra
+from helper import algebra2d
+from helper import algebra3d
 
 app = Flask(__name__)
 
 
-@app.route("/3D")
+@app.route("/3D", methods=['POST', 'GET'])
 def algebra3D_API():
-    dict = {'left': 0.17037454, 'right': 0.82339555, '_unknown_': 0.0059609693}
-    message = {
-        'status': 200,
-        'message': 'OK',
-        'scores': dict
+    # Request Structure:
+    #     equation
+    #     xStart
+    #     yStart
+    #     xEnd
+    #     yEnd
+    # Return Structure:
+    #     equation
+    #     latex
+    #     xValues
+    #     yValues
+    #     zValues
 
+    # Get Data
+    req = request.get_json(force=True)
+    print(req)
+
+    # Process Data
+    equation = algebra.getStringEq(req['equation'])
+    latex = algebra.getLatexEq(equation)
+    xStart = req['xStart']
+    xEnd = req['xEnd']
+    yStart = req['yStart']
+    yEnd = req['yEnd']
+
+    xValues, yValues, zValues = algebra3d.getZSurface(
+        equation, xStart, yStart, xEnd, yEnd)
+
+    # Package Data
+    resp = {
+        "equation": equation,
+        "latex": latex,
+        "xValues": xValues.astype(float).tolist(),
+        "yValues": yValues.astype(float).tolist(),
+        "zValues": zValues.astype(float).tolist()
     }
-    resp = jsonify(message)
-    resp.status_code = 200
+
     print(resp)
-    return resp
+
+    return jsonify(resp)
+
+
+@app.route("/2D", methods=['POST', 'GET'])
+def algebra2D_API():
+    # Request Structure:
+    #     Xequation
+    #     Yequation
+    #     start
+    #     end
+
+    # Return Structure:
+    #     equation
+    #     latex
+    #     xValues
+    #     yValues
+
+        # Get Data
+    req = request.get_json(force=True)
+    print(req)
+
+    # Process Data
+    Xequation = req['Xequation']
+    Yequation = req['Yequation']
+    start = req['start']
+    end = req['end']
+
+    if(Xequation != ""):
+        # Process as equation in terms of y
+        equation = algebra.getStringEq(Xequation)
+        xValues, yValues = algebra2d.getXYValuesFromXEq(equation, start, end)
+    else:
+        # Process as equation in terms of x
+        equation = algebra.getStringEq(Yequation)
+        xValues, yValues = algebra2d.getXYValuesFromYEq(equation, start, end)
+
+    latex = algebra.getLatexEq(equation)
+
+    # Package Data
+    resp = {
+        "equation": equation,
+        "latex": latex,
+        "xValues": xValues.astype(float).tolist(),
+        "yValues": yValues.astype(float).tolist(),
+    }
+
+    print(resp)
+
+    return jsonify(resp)
 
 
 @app.route("/")
